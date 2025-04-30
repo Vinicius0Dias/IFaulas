3fcdb21ad81565157acd83597ee267595f5a4dac
Vinicius0Dias <zedias007cake@outlook.com>
2025-04-29 22:28:43 -0300

    Add files via upload

---
+++ aula11ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+    int num[5] = {1,2,3,4,5} ;
+
+    printf("numero[0] = %d\n", num[0]);
+    printf("numero[1] = %d\n", num[1]);
+    printf("numero[2] = %d\n", num[2]);
+    printf("numero[3] = %d\n", num[3]);
+    printf("numero[4] = %d\n", num[4]);
+
+    return 0;
+}
+
